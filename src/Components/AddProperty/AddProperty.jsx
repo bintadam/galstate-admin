5e2f36dceda3d7601617bfbbf2889f41feb13cf5
bintadam <zakiyaadan12@gmail.com>
2024-04-20 16:28:38 +0200

    created function addproduct

--- src/Components/AddProperty/AddProperty.jsx
+++ src/Components/AddProperty/AddProperty.jsx
@@ -1,3 +1,4 @@
+import { response } from 'express'
 import { useState } from 'react'
 
 function AddProperty() {
@@ -41,6 +42,45 @@ function AddProperty() {
             }))
         }
     }
+
+    const addProduct = async () => {
+        let responseData;
+        let property = propertyDetails
+        let formData = new FormData()
+        formData.append('product', image) 
+        try{
+            await fetch('https://galstate-api-db8c5d80bbe4.herokuapp.com/uploadimage',{
+                method:'POST',
+                headers:{
+                    Accept:'application/json',
+                },
+                body:formData,
+            }).then((response) => response.json()).then((data) => {console.error('Error uploading image:', error)})
+
+            if(responseData && responseData.success){
+                property.image = responseData.image;
+                console.log(property)
+                await fetch('https://galstate-api-db8c5d80bbe4.herokuapp.com/addproperty', {
+                    method: 'POST',
+                    headers:{
+                        'Accept':'application/json',
+                        'Content-Type':'application/json',
+                    },
+                    body:JSON.stringify(property)
+                }).then((response)=>{
+                    if(response.ok && response.headers.get("Content-Type")?.includes('application/json')){
+                        return response.json()
+                    }
+                }).then((data) => {
+                    data.success ? alert('Property Added') : alert('Failed')
+                })
+            }
+           
+        } catch(error){
+            console.error('Error Uploading Property:', error)
+        }
+    }
+
     return (
         <div>
 
