c4f9ecb19bf01f7277dcb8c92d85d9a494242c83
bintadam <zakiyaadan12@gmail.com>
2024-04-20 17:15:33 +0200

    created function fetchdata for fetching data

--- src/Components/ListProperty/ListProperty.jsx
+++ src/Components/ListProperty/ListProperty.jsx
@@ -4,9 +4,19 @@ import crossicon from '../../assets/cross_icon.png'
 
 function ListProperty() {
     const [alproperties, setAllProperties] = useState([])
-    
+
+    const fetchData = async () => {
+        await fetch('https://galstate-api-db8c5d80bbe4.herokuapp.com/').then((response) => response.json()).then((data) => {setAllProperties(data)})
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
     return (
-        <div></div>
+        <div>
+
+        </div>
     )
 }
 
