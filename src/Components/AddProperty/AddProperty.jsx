50e70c94b6fb18bcf7e9ae652e5588227f151b87
bintadam <zakiyaadan12@gmail.com>
2024-04-20 17:08:26 +0200

    added addprperty css file

--- src/Components/AddProperty/AddProperty.jsx
+++ src/Components/AddProperty/AddProperty.jsx
@@ -6,7 +6,7 @@ function AddProperty() {
     const [images, setImages] = useState(false)
     const [propertyDetails, setPropertyDetails] = useState({
         description:"",
-        typ:"",
+        typ:"apartment",
         image:"",
         urlImages:[],
         category:{
@@ -20,6 +20,10 @@ function AddProperty() {
 
     const imageHandler = (e) => {
         setImage(e.target.files[0])
+    }
+
+    const arrImagesHandler = (e) => {
+        setImages(e.target.files[0])
     }
 
     const changeHandler = (e) => {
@@ -47,7 +51,8 @@ function AddProperty() {
         let responseData;
         let property = propertyDetails
         let formData = new FormData()
-        formData.append('product', image) 
+        formData.append('property', image)
+        formData.append('property', urlImages)
         try{
             await fetch('https://galstate-api-db8c5d80bbe4.herokuapp.com/uploadimage',{
                 method:'POST',
@@ -55,7 +60,15 @@ function AddProperty() {
                     Accept:'application/json',
                 },
                 body:formData,
-            }).then((response) => response.json()).then((data) => {console.error('Error uploading image:', error)})
+            }).then((response) => response.json()).then((error) => {console.error('Error uploading image:', error)})
+
+            await fetch('https://galstate-api-db8c5d80bbe4.herokuapp.com/uploadfeaturesimages',{
+                method:'post',
+                headers:{
+                    Accept:'application/json'
+                },
+                body:formData
+            }).then((response) => response.json()).then((error) => console.error('Error uploading image', error))
 
             if(responseData && responseData.success){
                 property.image = responseData.image;
@@ -82,8 +95,44 @@ function AddProperty() {
     }
 
     return (
-        <div>
-
+        <div className='add-product'>
+            <div className="addproduct-itemfield">
+                <p>Property description</p>
+                <textarea name="description" id="" cols="30" rows="10" onChange={changeHandler} value={propertyDetails.description}></textarea>
+            </div>
+            <div className="addproduct-type">
+                <p>Type</p>
+                <select name="ty" onChange={changeHandler}className="add-product-selector" value={propertyDetails.typ}>
+                    <option value="apartment">Apartment</option>
+                    <option value="house">House</option>
+                </select>
+            </div>
+            <div value={propertyDetails.category} className="addproduct-type">
+                <p>Category</p>
+                <select name="category" className="add-product-selector" onChange={changeHandler} >
+                    <option value='isSpecial'>isBuy</option>
+                    <option value='isPopular'>isRent</option>
+                </select>
+            </div>
+            <div value={propertyDetails.price} className="addproduct-price">
+                <p>Price</p>
+                <input type="text" name='price' onChange={changeHandler} placeholder='Type here'/>
+            </div>
+            <div value={propertyDetails.size} className="addproduct-price">
+                <p>Price</p>
+                <input type="text" name='size' onChange={changeHandler} placeholder='Type here'/>
+            </div>
+            <div value={propertyDetails.rooms} className="addproduct-price">
+                <p>Price</p>
+                <input type="text" name='rooms' onChange={changeHandler} placeholder='Type here'/>
+            </div>
+            <div>
+                <label htmlFor="">
+                    <img src={image ? URL.createObjectURL(image):upload_area} alt="" className='addproduct-thumbnail-img'/>
+                </label>
+                <input type="file" name='image' id='file-input' onChange={imageHandler} hidden/>
+            </div>
+            <button onClick={() => {AddProperty()}} className="addproduct-btn">ADD</button>
         </div>
     )
 }
